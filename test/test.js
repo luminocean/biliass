d56475c844afe5ad21c102c60b57a99c49ec7443
luminocean <282896922@qq.com>
2015-08-20 22:14:55 +0800

    弹幕布局基本实现

--- test/test.js
+++ test/test.js
@@ -5,5 +5,5 @@ var biliass = require('../lib/biliass');
 biliass.downloadAss('http://www.bilibili.com/video/av903236/',function(err, result){
   if(err) return console.error(err);
 
-  console.log(result);
+  //console.log(result);
 });
