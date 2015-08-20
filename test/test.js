ac91e54c2eaeca7012f72f97fdfa2f6d4249d145
luminocean <282896922@qq.com>
2015-08-20 10:25:50 +0800

    代码清理

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@
 
 var biliass = require('../lib/biliass');
 
-biliass.downloadAss('http://www.bilibili.com/video/av2619325/',function(err, result){
+biliass.downloadAss('http://www.bilibili.com/video/av903236/',function(err, result){
   if(err) return console.error(err);
 
   console.log(result);
